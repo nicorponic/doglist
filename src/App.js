3046174cd7ae6ae7ba9c0c7fecd00f31924d6ec3
nicorponic <n1co_frago@hotmail.com>
2019-01-23 18:02:46 +0100

    Add detail page for dogs, pass props for name and image to the link page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import logo from './logo.svg';
-import './App.css';
-import DogsListContainer from "./components/DogsListContainer"
+import "./App.css";
+import DogsListContainer from "./components/DogsListContainer";
+import { Route } from "react-router-dom";
+//import DogBreedImages from "./components/DogBreedImages"
+import DogBreedImagesContainer from "./components/DogBreedImagesContainer"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <main>
-          <DogsListContainer />
+          <Route exact path="/" component={DogsListContainer} />
+          <Route path="/dog-breeds/:breed" component={DogBreedImagesContainer} />
         </main>
       </div>
     );
