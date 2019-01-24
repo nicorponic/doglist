67f81e495fa28775449d9986c9843d8101d2e7e0
nicorponic <n1co_frago@hotmail.com>
2019-01-24 18:21:28 +0100

    Updates

---
+++ precache-manifest.57b020330e53f56a819d19b127e34089.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0c43c0b982c7b733909",
+    "url": "/doglist/static/css/main.fa99f516.chunk.css"
+  },
+  {
+    "revision": "a0c43c0b982c7b733909",
+    "url": "/doglist/static/js/main.a0c43c0b.chunk.js"
+  },
+  {
+    "revision": "c409e8a523a30617602f",
+    "url": "/doglist/static/js/1.c409e8a5.chunk.js"
+  },
+  {
+    "revision": "c8f99291a19dcf1453e0",
+    "url": "/doglist/static/js/runtime~main.c8f99291.js"
+  },
+  {
+    "revision": "7621c456fc6e0cbdfa4a631482cac97c",
+    "url": "/doglist/index.html"
+  }
+];
